76df97a494e7569d0c17588d427fe9ee3e08f8d0
Ray Rischpater <rischpater@google.com>
2018-11-20 12:26:57 -0800

    Adding missing files

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <webview_flutter/WebviewFlutterPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTWebviewFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTWebviewFlutterPlugin"]];
+}
+
+@end
